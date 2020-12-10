1135085fdb22cca652ec7b437a8331e9f6f1462a
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-12-10 20:13:13 +0100

    React app created, layout + contact page, styles

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const schema = yup.object().shape({
+    firstName: yup
+        .string()
+        .min(2, 'Name must conatin at least 2 characters')
+        .matches(/[a-zA-z-\s]/g, 'Only characters A-Z are valid')
+        .required(),
+    lastName: yup
+        .string()
+        .min(2, 'Name must conatin at least 2 characters')
+        .matches(/[a-zA-z-\s]/g, 'Only characters A-Z are valid')
+        .required(),
+    email: yup
+        .string()
+        .email('E-mail is not valid', { regex: /^[\d-_.a-zA-z]+@[\d-_.a-zA-z]+\.[a-zA-z]+$/g })
+        .required('E-mail is required'),
+    message: yup
+        .string()
+        .min(10, 'Message is required')
+        .required(),
+});
+
+function Contact() {
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(schema)
+    });
+  
+    function onSubmit(data) {
+        console.log("data", data);
+    }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" name="firstName" placeholder="Enter your first name" ref={register} />
+            <p className="error">{errors.firstName?.message}</p>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" name="lastName" placeholder="Enter your last name" ref={register} />
+            <p className="error">{errors.lastName?.message}</p>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control name="email" placeholder="Enter your email" ref={register} />
+            <p className="error">{errors.email?.message}</p>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Message</Form.Label>
+            <Form.Control type="message" name="message" placeholder="Enter your message" ref={register} />
+            <p className="error">{errors.message?.message}</p>
+        </Form.Group>
+
+        <Button type="submit">Submit</Button>
+    </form>
+  );
+}
+export default Contact;
---
+++ src/components/game/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from "../layout/Heading";
+
+export function Game() {
+    return (
+        <>
+            <Heading title="Game" />
+            <p>This is the Game page</p>
+        </>
+    );
+}
+
+export default Game;
